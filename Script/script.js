496314669a6507fd3245311a7fa95ac0abbdfcc3
Pedro Portatil <pedroj1229@gmail.com>
2025-04-13 17:24:14 -0500

    Pagina web Sofin

---
+++ Script/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const boton = document.getElementById("boton");
+
+    boton.addEventListener("click", function() {
+
+        window.open("http://3.230.129.132", "_blank");
+
+    })
+
+})
